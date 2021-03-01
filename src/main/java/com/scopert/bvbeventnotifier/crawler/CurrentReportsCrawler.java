7b725c4c5d0e0171eb7df8c5f1b73afcae312e7b
PopescuMarius <popescumariusvasile@yahoo.com>
2021-03-02 01:00:42 +0200

    REFACTORING: Extracted most jsoup/html code in separate class

--- src/main/java/com/scopert/bvbeventnotifier/crawler/CurrentReportsCrawler.java
+++ src/main/java/com/scopert/bvbeventnotifier/crawler/CurrentReportsCrawler.java
@@ -3,16 +3,12 @@ package com.scopert.bvbeventnotifier.crawler;
 import com.scopert.bvbeventnotifier.attachments.DocumentProcessor;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-
-import static com.scopert.bvbeventnotifier.utils.DateTimeUtils.getCurrentDateInBVBFormat;
-import static java.util.stream.Collectors.toCollection;
 
 @Slf4j
 @Service
@@ -25,53 +21,38 @@ public class CurrentReportsCrawler {
 
     //TODO 1. trebuie bagat postgresql sa pot lansa un MVP rapid. este gratis si pe AWS
 
-    private String lastProcessedReportOfTheDay = "Deputy Chief Executive Officer mandate termination";
+    private String lastProcessedReport = "Deputy Chief Executive Officer mandate termination";
 
     public void getLatestReportsOfToday(String URL) throws IOException {
-        Document document = Jsoup.connect(URL).get();
-        Elements reportsTable = document.select("table[id=gvv]");
-        Element tableContent = reportsTable.get(0).children().get(1);
+        DocumentWrapper bvbDocument = new DocumentWrapper(Jsoup.connect(URL).get());
+        String latestFoundReportDescription = bvbDocument.getLatestFoundReportDescription();
 
-        String latestFoundReport = getEventDescriptionFrom(tableContent.children().get(0));
-
-        if (lastProcessedReportOfTheDay.equals(latestFoundReport)) {
+        if (lastProcessedReport.equals(latestFoundReportDescription)) {
             log.info("Processed 0 reports. Index is up to date");
             return;
         }
 
-        Elements currentDayReports = tableContent.children()
-                                                 .stream()
-                                                 .filter(e -> e.child(3).text().startsWith(getCurrentDateInBVBFormat()))
-                                                 .collect(toCollection(Elements::new));
+        Elements currentDayReports = bvbDocument.getCurrentDayReportsForFollowedSymbols();
 
         for (Element row : currentDayReports) {
-            String symbol = row.child(0).select("strong").get(0).text();
-            String description = getEventDescriptionFrom(row);
-            String publishDate = row.child(3).text();
+            String symbol = bvbDocument.getEventSymbolFrom(row);
+            String description = bvbDocument.getEventDescriptionFrom(row);
 
-            if (lastProcessedReportOfTheDay.equals(description)) {
+            if (lastProcessedReport.equals(description)) {
                 break;
             }
 
-            if (TrackedSymbols.isTrackedSymbol(symbol)) {
-                System.out.println(publishDate + " " + symbol + " " + description);
-                Elements select = row.child(5).select("a[href]");
-                for (Element e : select) {
-                    String localeHref = e.attr("href");
-                    String url = HOSTNAME + localeHref;
-                    if (isRomanianFile(url)) {
-                        documentProcessor.processAttachments(symbol, url);
-                    }
+            Elements attachments = bvbDocument.getAttachmentsFrom(row);
+            for (Element attachment : attachments) {
+                String url = bvbDocument.computeURLfromBVBPath(attachment);
+                if (isRomanianFile(url)) {
+                    documentProcessor.processAttachments(symbol, url);
                 }
             }
         }
 
-        lastProcessedReportOfTheDay = latestFoundReport;
+        lastProcessedReport = latestFoundReportDescription;
         log.info("Processed {} new reports. Index has been updated with last processed report.", currentDayReports.size());
-    }
-
-    private String getEventDescriptionFrom(Element row) {
-        return row.child(2).attr("data-search");
     }
 
     /* Most basic mode to eliminate english files. Maybe there are other languages or files that are not useful
---
+++ src/main/java/com/scopert/bvbeventnotifier/crawler/DocumentWrapper.java
@@ -0,0 +1,56 @@
+package com.scopert.bvbeventnotifier.crawler;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import static com.scopert.bvbeventnotifier.crawler.CurrentReportsCrawler.HOSTNAME;
+import static com.scopert.bvbeventnotifier.utils.DateTimeUtils.getCurrentDateInBVBFormat;
+import static java.util.stream.Collectors.toCollection;
+
+public class DocumentWrapper {
+
+    private Document wrappedDocument;
+
+    public DocumentWrapper(Document document) {
+        this.wrappedDocument = document;
+    }
+
+    public String getLatestFoundReportDescription() {
+        Element tableContent = getTableContent();
+        return getEventDescriptionFrom(tableContent.children().get(0));
+    }
+
+    public Elements getCurrentDayReportsForFollowedSymbols() {
+        return getTableContent()
+                .children()
+                .stream()
+                .filter(e -> e.child(3).text().startsWith(getCurrentDateInBVBFormat()))
+                .filter(e -> TrackedSymbols.isTrackedSymbol(e.child(0).select("strong").get(0).text()))
+                .collect(toCollection(Elements::new));
+    }
+
+    public String getEventDescriptionFrom(Element row) {
+        return row.child(2).attr("data-search");
+    }
+
+    public String getEventSymbolFrom(Element row) {
+        return row.child(0).select("strong").get(0).text();
+    }
+
+    public Elements getAttachmentsFrom(Element row) {
+        return row.child(5).select("a[href]");
+    }
+
+    public String computeURLfromBVBPath(Element attachment) {
+        String localeHref = attachment.attr("href");
+        String url = HOSTNAME + localeHref;
+        return url;
+    }
+
+    private Element getTableContent() {
+        Elements reportsTable = wrappedDocument.select("table[id=gvv]");
+        Element tableContent = reportsTable.get(0).children().get(1);
+        return tableContent;
+    }
+}
