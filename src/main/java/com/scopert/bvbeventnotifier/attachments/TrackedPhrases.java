4d951df57b661ce23fd0173a179a77f596b960c5
PopescuMarius <popescumariusvasile@yahoo.com>
2021-03-15 11:44:16 +0200

    Added new tracked phrase for aquisition

--- src/main/java/com/scopert/bvbeventnotifier/attachments/TrackedPhrases.java
+++ src/main/java/com/scopert/bvbeventnotifier/attachments/TrackedPhrases.java
@@ -6,26 +6,25 @@ import java.util.Optional;
 
 @Getter
 public enum TrackedPhrases {
-    //1. contract mare
+
     SIGNIFICANT_CONTRACT("contract semnificativ"),
-    //2. rezultate bune
-    MARJA_RECORD("marjă record"),
-    PERFORMANTA_RECORD("o performanță record"),
-    RANDAMENT_RECORD("randament record"),
-    //3.
+    PERFORMANTA_RECORD1("marjă record"),
+    PERFORMANTA_RECORD2("o performanță record"),
+    PERFORMANTA_RECORD3("randament record"),
     ACTIUNI_GRATUITE1("acțiuni gratuite"),
     ACTIUNI_GRATUITE2("actiuni gratuite"),
-    //4. majorare capital
-    RATA_DE_SUBSCRIERE("Rata de subscriere"),
+    ACTIUNI_GRATUITE3("Rata de subscriere"),
     MAJORARE_DE_CAPITAL1("majorare de capital"),
     MAJORARE_DE_CAPITAL2("majorarii capitalului social"),
-    //5 DIVIDENDe
     DIVIDEND_SPECIAL("dividend special"),
-    //6 Achizitie companii
     ACHIZITIE_COMPANIE_1A("promisiuni bilaterale de achiziționare"),
     ACHIZITIE_COMPANIE_1B("promisiuni bilaterale de achizitionare"),
     ACHIZITIE_COMPANIE_2A("dobândirea tuturor părților sociale"),
-    ACHIZITIE_COMPANIE_2B("dobandirea tuturor partilor sociale");
+    ACHIZITIE_COMPANIE_2B("dobandirea tuturor partilor sociale"),
+    //TODO de studiat open-close price si data mesajului primit pentru M in data de 15.03
+    //TODO cred ca ma va interesa si rulajul mediu, sa imi dau seama cat as putea sa cumpar
+    ACHIZITIE_COMPANIE_3A("aceasta achizitie"),
+    ACHIZITIE_COMPANIE_3B("această achiziție");
 
     //TODO in baza tb sa am si evenimente asemanatoare -> data si ora, pretul inchidere pe urmatoarele 3 zile etc sa imi pot face o idee daca merita sau nu luat
     private String value;
